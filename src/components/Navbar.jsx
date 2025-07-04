3042a230ce29361a6607a30bcf8dda1beb5ce457
Héctor Fernández <60543953+Porki5@users.noreply.github.com>
2025-07-04 13:08:11 +0200

    a

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
+import LogoSW from "../assets/img/logosw.png";
+
+
+
+
 
 export const Navbar = () => {
 
 	return (
 		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+			<div className="container d-flex justify-content-between">
+				<Link to="/" className="logo mw-3">
+					<img src ={LogoSW}  style={{ maxWidth: "120px", height: "auto" }} />
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+					
+					<button className="favoritos btn btn-primary">Favoritos</button>
+					
 				</div>
 			</div>
 		</nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,21 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import {Planetas} from "../components/Planetas.jsx";
+import {Personajes} from "../components/Personajes.jsx";
 
 export const Home = () => {
 
   const {store, dispatch} =useGlobalReducer()
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+		<div className="container text-center mt-5">
+			<h1>Bienvenidos a mi blog de Star Wars</h1>
+			<div className="contPersonajes d-flex flex-wrap mt-4">
+				<Personajes />
+			</div>
+			<div className="contPlanetas d-flex flex-wrap mt-4">
+				<Planetas />
+			</div>
+
 		</div>
 	);
 }; 
